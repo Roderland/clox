375d53ca54bcd8fc22304cef9029e25787e9de2a
roderland <weiguo_feng@foxmail.com>
2022-05-16 15:22:16 +0800

    chapter-03

---
+++ chapter-03/src/compiler.c
@@ -0,0 +1,26 @@
+//
+// Created by roderland on 2022/5/16.
+//
+
+#include <stdio.h>
+
+#include "common.h"
+#include "compiler.h"
+#include "scanner.h"
+
+void compile(const char* source) {
+    initScanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
+}
---
+++ chapter-03/src/compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by roderland on 2022/5/16.
+//
+
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+void compile(const char* source);
+
+#endif
