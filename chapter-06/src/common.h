8be98771ebbba2679489836e6bea6e98b0d0b8db
roderland <weiguo_feng@foxmail.com>
2022-05-18 14:09:13 +0800

    chapter-06

---
+++ chapter-06/src/common.h
@@ -0,0 +1,15 @@
+//
+// Created by roderland on 2022/5/16.
+//
+
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+
+#endif
