d2684ef69a7c657a6d8f9a9c57cf8149309dfa96
roderland <weiguo_feng@foxmail.com>
2022-05-19 11:11:16 +0800

    chapter-09

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CHAPTER = chapter-08
+CHAPTER = chapter-09
 
 clox:
 	cc -std=c99 -g -Wall -Wunused-function $(CHAPTER)/src/* -lm -o clox
---
+++ chapter-09/src/compiler.h
@@ -0,0 +1,13 @@
+//
+// Created by roderland on 2022/5/16.
+//
+
+#ifndef clox_compiler_h
+#define clox_compiler_h
+
+#include "object.h"
+#include "vm.h"
+
+bool compile(const char* source, Chunk* chunk);
+
+#endif
