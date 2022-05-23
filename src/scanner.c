f1cec874bb0ea6a2b26299f0b754a80929b49799
roderland <weiguo_feng@foxmail.com>
2022-05-23 22:43:22 +0800

    "+=", "-=", "*=", "/="

--- src/scanner.c
+++ src/scanner.c
@@ -201,10 +201,18 @@ Token scanToken() {
         case ';': return makeToken(TOKEN_SEMICOLON);
         case ',': return makeToken(TOKEN_COMMA);
         case '.': return makeToken(TOKEN_DOT);
-        case '-': return makeToken(TOKEN_MINUS);
-        case '+': return makeToken(TOKEN_PLUS);
-        case '/': return makeToken(TOKEN_SLASH);
-        case '*': return makeToken(TOKEN_STAR);
+        case '-':
+            return makeToken(
+                    match('=') ? TOKEN_MINUS_EQUAL : TOKEN_MINUS);
+        case '+':
+            return makeToken(
+                    match('=') ? TOKEN_PLUS_EQUAL : TOKEN_PLUS);
+        case '/':
+            return makeToken(
+                    match('=') ? TOKEN_SLASH_EQUAL : TOKEN_SLASH);
+        case '*':
+            return makeToken(
+                    match('=') ? TOKEN_STAR_EQUAL : TOKEN_STAR);
         case '!':
             return makeToken(
                     match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
